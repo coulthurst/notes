f12c897aa1bf4c6e61047b5ae464d82db8e39f15
Cole Schmitt <cschmitt@ipminstitute.org>
2019-02-18 22:30:40 -0600

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "font-awesome/css/font-awesome.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.querySelector("#root"));
